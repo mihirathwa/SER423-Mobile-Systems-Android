19c8b3e0540a53e7bfbd64c747bd2093a19aee40
Mihir Rathwa <mihir.rathwa@gmail.com>
2017-01-18 16:30:52 -0700

    Updated licence

--- app/src/main/java/edu/asu/msse/mrathwa/placeman/MainActivity.java
+++ app/src/main/java/edu/asu/msse/mrathwa/placeman/MainActivity.java
@@ -3,23 +3,11 @@ package edu.asu.msse.mrathwa.placeman;
 /*
  * Copyright 2017 Mihir Rathwa,
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * This license also provides instructor Dr. Tim Lindquist and Arizona
+ * This license provides the instructor Dr. Tim Lindquist and Arizona
  * State University the right to build and evaluate the package for the
  * purpose of determining grade and program assessment.
  *
- * Purpose: This class contains description about the place as described
+ * Purpose: This class contains the connection between Place Description and UI layout
  * in Assignment 1
  *
  * Ser423 Mobile Applications
